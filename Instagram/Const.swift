33a7bbb362e853907201271c36e40eab94c8e72a
yuuya.oosugi <mande_e@ezweb.ne.jp>
2021-10-07 18:56:32 +0900

    質問②

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 大杉祐弥 on 2021/10/05.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPath = "posts"
+}
--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import SVProgressHUD
 
 class LoginViewController: UIViewController {
     
@@ -16,6 +17,33 @@ class LoginViewController: UIViewController {
     
     //ログインボタンをタップした時に呼ばれるメソッド
     @IBAction func handleLoginButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text {
+            
+            //アドレスとパスワード名のいずれかでも入力されていない時は何もしない
+            if address.isEmpty || password.isEmpty {
+                SVProgressHUD.showError(withStatus: "必要項目を入力してください。")
+                return
+            }
+            
+            //HUDで処理中を表示
+            SVProgressHUD.show()
+            
+            Auth.auth().signIn(withEmail: address, password: password) { authResult, error in
+                if let error = error {
+                    print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "サインインに失敗しました。")
+                    return
+                }
+                print("DEBUG_PRINT: ログインに成功しました。")
+                
+                //HUDを消す
+                SVProgressHUD.dismiss()
+                
+                //画面を閉じてタブ画面に戻る
+                self.dismiss(animated: true, completion: nil)
+                
+            }
+        }
     }
     
     //アカウント作成ボタンをタップした時に呼ばれるメソッド
@@ -25,8 +53,12 @@ class LoginViewController: UIViewController {
             //アドレスとパスワードと表示名のいずれかでも入力されてない場合は何もしない
             if address.isEmpty || password.isEmpty || displayName.isEmpty {
                 print("DEBUG_PRINT: 何かが空文字です。")
+                SVProgressHUD.showError(withStatus: "必要項目を入力してください。")
                 return
             }
+            
+            //HUDで処理中を表示
+            SVProgressHUD.show()
             
             //アドレスとパスワードでユーザー作成。作成に成功すると自動的にログインする
             Auth.auth().createUser(withEmail: address, password: password) { authResult, error in
@@ -35,6 +67,7 @@ class LoginViewController: UIViewController {
                     
                     //エラーがあったら原因をprintして、returnすることで以降の処理を実行せずに処理を終了する
                     print("DEBUG_PRINT: " + error.localizedDescription)
+                    SVProgressHUD.showError(withStatus: "ユーザー作成に失敗しました。")
                     return
                 }
                 print("DEBUG_PRINT: ユーザー作成に成功しました。")
@@ -48,9 +81,13 @@ class LoginViewController: UIViewController {
                         if let error = error {
                             //プロフィールの更新でエラーが発生
                             print("DEBUG_PRINT: " + error.localizedDescription)
+                            SVProgressHUD.showError(withStatus: "表示名の設定に失敗しました。")
                             return
                         }
                         print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました。")
+                        
+                        //HUDを消す
+                        SVProgressHUD.dismiss()
                         
                         //画面を閉じてタブ画面に戻る
                         self.dismiss(animated: true, completion: nil)
